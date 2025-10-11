69618601db68b2ce3e55a1f6f789e11775291cb3
sameedsaeed123 <l1f22bscs0956@ucp.edu.pk>
2025-10-12 04:24:22 +0500

    Update API_URL to a specific endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // Centralized API URL for the frontend to consume. Uses the .env REACT_APP_API_URL
-const API_URL = process.env.REACT_APP_API_URL || '';
+const API_URL = 'https://techtrust-d7b4hge2hxgtfwer.centralindia-01.azurewebsites.net';
 
 export default API_URL;
